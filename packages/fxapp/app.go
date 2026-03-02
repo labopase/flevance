949632be591f57051b779a12320e9425d71351e2
halimdotnet <halimdotnet@gmail.com>
2026-03-02 22:50:18 +0700

    update
    Signed-off-by: halimdotnet <halimdotnet@gmail.com>

--- packages/fxapp/app.go
+++ packages/fxapp/app.go
@@ -1,9 +1,35 @@
 package fxapp
 
 import (
+	"context"
+
 	"go.uber.org/fx"
 )
 
-type App struct {
-	fx.App
+type app struct {
+	fxApp *fx.App
+}
+
+type App interface {
+	Start(ctx context.Context) error
+	Stop(ctx context.Context) error
+	Wait() <-chan fx.ShutdownSignal
+}
+
+func NewApp(opts ...fx.Option) App {
+	return &app{
+		fxApp: fx.New(opts...),
+	}
+}
+
+func (a *app) Start(ctx context.Context) error {
+	return a.fxApp.Start(ctx)
+}
+
+func (a *app) Stop(ctx context.Context) error {
+	return a.fxApp.Stop(ctx)
+}
+
+func (a *app) Wait() <-chan fx.ShutdownSignal {
+	return a.fxApp.Wait()
 }
--- packages/fxapp/builder.go
+++ packages/fxapp/builder.go
@@ -1,8 +1,44 @@
 package fxapp
 
-import "go.uber.org/fx"
+import (
+	"slices"
+
+	"go.uber.org/fx"
+)
 
 type builder struct {
 	options  []fx.Option
 	invokers []fx.Option
+}
+
+type Builder interface {
+	Option(opts ...fx.Option) Builder
+	Invoker(invokers ...interface{}) Builder
+	Build() App
+}
+
+func NewBuilder() Builder {
+	return &builder{
+		options: []fx.Option{
+			fx.NopLogger,
+		},
+		invokers: make([]fx.Option, 0),
+	}
+}
+
+func (b *builder) Option(opts ...fx.Option) Builder {
+	b.options = append(b.options, opts...)
+	return b
+}
+
+func (b *builder) Invoker(invokers ...interface{}) Builder {
+	for _, invoker := range invokers {
+		b.invokers = append(b.invokers, fx.Invoke(invoker))
+	}
+	return b
+}
+
+func (b *builder) Build() App {
+	options := slices.Concat(b.options, b.invokers)
+	return NewApp(options...)
 }
